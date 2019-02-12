d51733efd150487eabe16a52c77452231ae8e9d9
unknown <PaliwalSparsh@Sparsh-PC.localdomain>
2019-02-13 00:02:03 +0530

    component: Create worm component

---
+++ src/games/Diglett.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Worm from "./components/Worm.js";
+import posed from "react-pose";
+import "./styles.css";
+
+function Diglett() {
+  const [states, setStates] = useState([0, 1, 1, 1]);
+
+  useEffect(() => {
+      const generateStates = setInterval(()=>{
+        let updatedState = [];
+        const randomString = String(parseInt(10000 * Math.random()));
+        for (let charIndex = 0; charIndex < randomString.length; charIndex++) {
+            updatedState.push(parseInt(Number(randomString[charIndex])/5));
+        }
+        setStates(updatedState)
+      }, 2000);
+      return ()=>{
+          clearInterval(generateStates);
+      }
+  });
+
+  return (
+    <div className="container">
+      {states.map((state, index) => (
+        <Worm pose={state} key={index} />
+      ))}
+    </div>
+  );
+}
+
+export default Diglett;
