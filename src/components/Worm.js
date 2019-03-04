e6997f303889958af116277a789c705b0c2a1262
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2019-03-05 00:18:22 +0530

    Fix worm component and add storybooks

--- src/components/Worm.js
+++ src/components/Worm.js
@@ -1,56 +1,48 @@
-import React from "react";
-import posed from "react-pose";
-import "./styles/Worm.style.css";
+import React from 'react';
+import posed from 'react-pose';
+import './styles/Worm.style.css';
 
 const poses = {
-  0: "inside",
-  1: "outside",
-  2: "destroy"
+	0: 'inside',
+	1: 'outside',
+	2: 'destroy',
 };
 
 const WormBody = posed.div({
-  inside: {
-    scaleX: 1,
-    scaleY: 0,
-    originX: "50%",
-    originY: "100%"
-  },
-  outside: {
-    scaleX: 1,
-    scaleY: 1,
-    originX: "50%",
-    originY: "100%"
-  },
-  destroy: {
-    scaleX: 0,
-    scaleY: 0,
-    originX: "50%",
-    originY: "50%",
-    transition: {
-      duration: 100
-    }
-  }
+	inside: {
+		scaleX: 1,
+		scaleY: 0,
+		originX: '50%',
+		originY: '100%',
+	},
+	outside: {
+		scaleX: 1,
+		scaleY: 1,
+		originX: '50%',
+		originY: '100%',
+	},
+	destroy: {
+		scaleX: 0,
+		scaleY: 0,
+		originX: '50%',
+		originY: '50%',
+		transition: {
+			duration: 100,
+		},
+	},
 });
 
 function Worm(props) {
-  const { index, wormState, updateWormState } = props;
-  function killWorm() {
-    updateWormState(index, 2);
-  }
-  return (
-    <WormBody
-      className="worm-body"
-      onClick={killWorm}
-      onTouch={killWorm}
-      pose={poses[wormState]}
-    >
-      <div className="worm-body__eyes">
-        <div className="worm-body__eye" />
-        <div className="worm-body__eye" />
-      </div>
-      <div className="worm-body__tongue" />
-    </WormBody>
-  );
+	const { state, onClick } = props;
+	return (
+		<WormBody className="worm-body" onClick={onClick} pose={poses[state]}>
+			<div className="worm-body__eyes">
+				<div className="worm-body__eye" />
+				<div className="worm-body__eye" />
+			</div>
+			<div className="worm-body__tongue" />
+		</WormBody>
+	);
 }
 
 export default Worm;
--- src/components/WormContainer.js
+++ src/components/WormContainer.js
@@ -1,76 +1,84 @@
-import React, { useContext, useEffect, useState } from "react";
-import { GameData } from "../contexts/GameData";
-import Worm from "./Worm";
-import "./styles/WormContainer.style.css";
+import React, { useContext, useEffect, useState } from 'react';
+import { GameData } from '../contexts/GameData';
+import Worm from './Worm';
+import './styles/WormContainer.style.css';
 
 const WORMS_SPAWNING_PER_TURN = 2;
+const WORM_DESTROY_STATE = 2;
 let oddInterval = true;
 
 function generateWormStates() {
-  const allPossibleStates = [[0, 0, 1, 1], [0, 1, 0, 1], [1, 0, 0, 1], [1, 0, 1, 0], [1, 1, 0, 0], [0, 1, 1, 0]];
-  const randomState = allPossibleStates[Math.floor(Math.random() * allPossibleStates.length)];
-  const updatedState = [...randomState];
-  return updatedState;
+	const allPossibleStates = [
+		[0, 0, 1, 1],
+		[0, 1, 0, 1],
+		[1, 0, 0, 1],
+		[1, 0, 1, 0],
+		[1, 1, 0, 0],
+		[0, 1, 1, 0],
+	];
+	const randomState = allPossibleStates[Math.floor(Math.random() * allPossibleStates.length)];
+	const updatedState = [...randomState];
+	return updatedState;
 }
 
 export default function WormContainer(props) {
-  const [wormStates, setWormStates] = useState([0, 1, 1, 1]);
-  const [wormCount, setWormCount] = useState(0);
-  const { dispatch, state } = useContext(GameData);
-  const { intervalTime, wormSpawnsPerGame } = props
+	const [wormStates, setWormStates] = useState([0, 1, 1, 1]);
+	const [wormCount, setWormCount] = useState(0);
+	const { dispatch, state } = useContext(GameData);
+	const { intervalTime, wormSpawnsPerGame } = props;
 
-  useEffect(() => {
-    const generateStates = setInterval(() => {
-      let updatedState;
-      if (oddInterval) {
-        updatedState = generateWormStates();
-        const updatedWormCount = wormCount + WORMS_SPAWNING_PER_TURN;
-        setWormCount(updatedWormCount);
-        updateProgress(updatedWormCount);
-      } else {
-        updatedState = [0, 0, 0, 0];
-      }
-      oddInterval = !oddInterval;
-      setWormStates(updatedState);
-    }, intervalTime);
-    return () => {
-      clearInterval(generateStates);
-    };
-  });
+	useEffect(() => {
+		const generateStates = setInterval(() => {
+			let updatedState;
+			if (oddInterval) {
+				updatedState = generateWormStates();
+				const updatedWormCount = wormCount + WORMS_SPAWNING_PER_TURN;
+				setWormCount(updatedWormCount);
+				updateProgress(updatedWormCount);
+			} else {
+				updatedState = [0, 0, 0, 0];
+			}
+			oddInterval = !oddInterval;
+			setWormStates(updatedState);
+		}, intervalTime);
+		return () => {
+			clearInterval(generateStates);
+		};
+	});
 
-  function updateProgress() {
-    dispatch({
-      type: "UPDATE_PROGRESS",
-      payload: { progress: (wormCount / wormSpawnsPerGame) * 100 }
-    });
-  }
+	function updateProgress() {
+		dispatch({
+			type: 'UPDATE_PROGRESS',
+			payload: { progress: (wormCount / wormSpawnsPerGame) * 100 },
+		});
+	}
 
-  function updateWormState(index, wormState) {
-    const updatedWormStates = [...wormStates];
-    updatedWormStates[index] = wormState;
-    setWormStates(updatedWormStates);
-  }
+	function updateWormState(index, wormState) {
+		const updatedWormStates = [...wormStates];
+		updatedWormStates[index] = wormState;
+		setWormStates(updatedWormStates);
+	}
 
-  function updateStatesAndScores(index, wormState) {
-    if (wormState === 2) {
-      dispatch({
-        type: "UPDATE_CURRENT_SCORE",
-        payload: { currentScore: state.currentScore + 1 }
-      });
-    }
-    updateWormState(index, wormState);
-  }
+	function killWorm(index) {
+		return function() {
+			dispatch({
+				type: 'UPDATE_CURRENT_SCORE',
+				payload: { currentScore: state.currentScore + 1 },
+			});
+			updateWormState(index, WORM_DESTROY_STATE);
+		};
+	}
 
-  return (
-    <div className="worm-container">
-      {wormStates.map((wormState, index) => (
-        <Worm
-          wormState={wormState}
-          updateWormState={updateStatesAndScores}
-          index={index}
-          key={index}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className="worm-container">
+			{wormStates.map((wormState, index) => (
+				<Worm
+					state={wormState}
+					onClick={killWorm(index)}
+					index={index}
+					key={index}
+				/>
+			))}
+		</div>
+	);
 }
