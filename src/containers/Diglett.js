3f968557301c8e0fc713551e936d55c92e63c8a2
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2019-03-12 00:34:33 +0530

    refactor(GameContainer.js): Move GameContainer code to DiglettContainer
    Moved code so that we can create generic component to hold game with the name GameContainer.
    Currently the GameContainer had code specific to diglett.

--- src/containers/Diglett.js
+++ src/containers/Diglett.js
@@ -1,14 +1,14 @@
 import React from "react";
-import GameContainer from "../components/GameContainer";
+import DiglettContainer from "../components/DiglettContainer";
 import WormContainer from "../components/WormContainer";
 import "./styles/Diglett.style.css";
 
 function Diglett(props) {
   const { intervalTime, wormSpawnsPerGame } = props;
   return (
-    <GameContainer displayName="Diglett">
+    <DiglettContainer displayName="Diglett">
       <WormContainer intervalTime={intervalTime} wormSpawnsPerGame={wormSpawnsPerGame} />
-    </GameContainer>
+    </DiglettContainer>
   );
 }
 
