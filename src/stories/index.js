9d2a1a8291f79bebf4d5d19b8fcec22d00493ad4
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2019-03-12 01:11:09 +0530

    feat(GameContainer.js): Add component to hold the game
    This component will be the top most component inside which all other components will reside
    
    Fixes #23

--- src/stories/index.js
+++ src/stories/index.js
@@ -9,6 +9,7 @@ import ProgressBar from '../components/ProgressBar';
 import Score from '../components/Score';
 import Worm from '../components/Worm';
 import StartMenu from '../components/StartMenu';
+import GameContainer from '../components/GameContainer';
 
 addDecorator(withKnobs);
 
@@ -47,16 +48,41 @@ storiesOfWorm.add('default', () => {
 //---------------------------------------------------------------------------//
 
 const storiesOfStartMenu = storiesOf('StartMenu', module);
-storiesOfStartMenu.add('default', () => {
+const StartMenuComponent = () => (
+	<StartMenu
+		title={text('Title', 'The Game of Life')}
+		onClickStart={action('start click')}
+		noteForSmallScreens={text('Note for small screens', 'Do this on a small screen.')}
+	>
+		Thats all folks
+	</StartMenu>
+);
+
+storiesOfStartMenu.add('with game container', () => {
+	return (
+		<GameContainer note="Let's see how this looks inside game container.">
+			<StartMenuComponent />
+		</GameContainer>
+	);
+});
+
+storiesOfStartMenu.add('without game container', () => {
 	return (
 		<div style={{ height: 440 }}>
-			<StartMenu
-				title={text('Title', 'The Game of Life')}
-				onClickStart={action('start click')}
-				noteForSmallScreens={text('Note for small screens', 'Do this on a small screen.')}
-			>
-				Thats all folks
-			</StartMenu>
+			<StartMenuComponent />
 		</div>
 	);
 });
+
+//---------------------------------------------------------------------------//
+
+const storiesOfGameContainer = storiesOf('GameContainer', module);
+storiesOfGameContainer.add('default', () => {
+	return (
+		<GameContainer note={text('Note', 'Play this game at your own risk.')}>
+			Place Game in here.
+		</GameContainer>
+	);
+});
+
+//---------------------------------------------------------------------------//
